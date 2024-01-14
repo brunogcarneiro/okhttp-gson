20f27d0ec464394d5b17fc218ce8875c27b76721
Bruno Carneiro <bcarneiro@ip-192-168-0-58.eu-central-1.compute.internal>
2024-01-14 14:06:10 -0300

    project created

---
+++ src/main/java/carneiro/bruno/App.java
@@ -0,0 +1,13 @@
+package carneiro.bruno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
